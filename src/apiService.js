2f3b7cec06d141187585fc703d29508a7f26682b
Siddhanta Mallick <sid.mallick1947@gmail.com>
2026-03-27 19:43:48 +0530

    Fix backend addMaterial endpoint to end with /, django does not tolerate this for some reason

--- src/apiService.js
+++ src/apiService.js
@@ -10,7 +10,7 @@ const API_BASE_URL = import.meta.env.VITE_BACKEND_API_BASE_URL;
 export const apiService = {
 
     addMaterial: async (config) => {
-        const response = await axios.post(`${API_BASE_URL}/${config.subjecttoLowerCase()}/add`, {
+        const response = await axios.post(`${API_BASE_URL}/${config.subject.toLowerCase()}/add/`, {
             title: `${config.title}`,
             content: `${config.content}`,
         });
