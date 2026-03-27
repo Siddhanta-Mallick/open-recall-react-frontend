868bc59e0c84320510a0fdc08a385c6792222b6b
Siddhanta Mallick <sid.mallick1947@gmail.com>
2026-03-27 18:30:09 +0530

    change api service to convet subject name to lower case

--- src/apiService.js
+++ src/apiService.js
@@ -10,7 +10,7 @@ const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 export const apiService = {
 
     addMaterial: async (config) => {
-        const response = await axios.post(`${API_BASE_URL}/${config.subject}/add`, {
+        const response = await axios.post(`${API_BASE_URL}/${config.subjecttoLowerCase()}/add`, {
             title: `${config.title}`,
             content: `${config.content}`,
         });
